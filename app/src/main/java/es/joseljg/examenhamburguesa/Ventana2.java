ba792c99ab2cc970c4bffb6e26a95380c72436a1
joseljg <joseljg@gmail.com>
2021-10-15 21:10:22 +0200

    Initial commit

---
+++ app/src/main/java/es/joseljg/examenhamburguesa/Ventana2.java
@@ -0,0 +1,14 @@
+package es.joseljg.examenhamburguesa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ventana2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana2);
+    }
+}
